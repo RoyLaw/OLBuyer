c8209628d456898a73d203472284948d503ddba0
Roy Law <roy_law@126.com>
2015-01-20 18:04:31 +0800

    init

--- Unit1.pas
+++ Unit1.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes, Vcl.Graphics,StrUtils,
+  System.Classes, Vcl.Graphics, StrUtils,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, IdIOHandler, IdIOHandlerSocket,
   IdIOHandlerStack, IdSSL, IdSSLOpenSSL, IdBaseComponent, IdComponent,
   IdTCPConnection, IdTCPClient, IdHTTP, Vcl.StdCtrls, IdCookieManager,
@@ -20,8 +20,17 @@ type
     Memo2: TMemo;
     IdCookieManager1: TIdCookieManager;
     Button2: TButton;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Edit4: TEdit;
+    Button3: TButton;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -37,8 +46,8 @@ implementation
 
 procedure TForm1.Button1Click(Sender: TObject);
 var
-  Params :TStrings;
-  i :integer;
+  Params: TStrings;
+  i: integer;
 begin
   IdSSLIOHandlerSocketOpenSSL1.SSLOptions.Method := sslvSSLv3;
   IdHTTP1.IOHandler := IdSSLIOHandlerSocketOpenSSL1;
@@ -54,20 +63,28 @@ begin
   IdHTTP1.Request.Connection := 'keep-alive';
 
   // Proxy Setting
-//   IdHTTP1.ProxyParams.ProxyServer := '127.0.0.1';
-//   IdHTTP1.ProxyParams.ProxyPort := 8080;
+  // IdHTTP1.ProxyParams.ProxyServer := '127.0.0.1';
+  // IdHTTP1.ProxyParams.ProxyPort := 8080;
 
-//  Memo1.Text := IdHTTP1.Get(Edit1.Text);
+  // Memo1.Text := IdHTTP1.Get(Edit1.Text);
 
   Params := TStringList.Create;
-  Params.Add('login=only4reg@126.com');
+  Params.Add('login=' + Edit2.Text);
   Params.Add('rememberMe=false');
-  Params.Add('password=NikePlus360');
+  Params.Add('password=' + Edit3.Text);
 
-  Memo1.Text := IdHTTP1.Post(Edit1.Text, Params);
+  try
+    Memo1.Text := IdHTTP1.Post(Edit1.Text, Params);
+  except
+    on E: Exception do
+    begin
+      Memo1.Text := 'Not Logged In.' + E.Message;
+    end;
+  end;
 
-  for i := 0 to IdHTTP1.CookieManager.CookieCollection.Count -1 do
-  Memo2.Text := Memo2.Text + #13#10 +IdHTTP1.CookieManager.CookieCollection.Cookies[i].ClientCookie;
+  for i := 0 to IdHTTP1.CookieManager.CookieCollection.Count - 1 do
+    Memo2.Text := Memo2.Text + #13#10 + IdHTTP1.CookieManager.CookieCollection.
+      Cookies[i].ClientCookie;
 
 end;
 
@@ -75,5 +92,29 @@ procedure TForm1.Button2Click(Sender: TObject);
 begin
   Application.Terminate;
 end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  ResponseData: TStringStream;
+  i: integer;
+begin
+  try
+    IdHTTP1.Request.Referer := 'http://store.nike.com/cn/zh_cn/pd/';
+    Memo1.Text := IdHTTP1.Get
+      ('https://secure-store.nike.com/ap/services/jcartService?callback=nike_Cart_hanleJCartResponse&action=addItem&lang_locale=zh_CN&country=CN&catalogId=4&productId='
+      + Edit4.Text +
+      '&price=1699.0&siteId=null&line1=Air+Jordan+XX9&line2=%E7%94%B7%E5%AD%90%E7%AF%AE%E7%90%83%E9%9E%8B&passcode=null&sizeType=null&skuAndSize=10702226%3A40.5&qty=1&rt=&view=3&skuId=10702226&displaySize=40.5&_=1420792220969');
+  except
+    on E: Exception do
+    begin
+      Memo1.Text := 'ERR:' + E.Message;
+    end;
+  end;
+    for i := 0 to IdHTTP1.CookieManager.CookieCollection.Count - 1 do
+    Memo2.Text := Memo2.Text + #13#10 + IdHTTP1.CookieManager.CookieCollection.
+      Cookies[i].ClientCookie;
+end;
+
+
 
 end.
---
+++ Unit2.dfm
@@ -0,0 +1,96 @@
+object Form2: TForm2
+  Left = 0
+  Top = 0
+  Caption = 'Form2'
+  ClientHeight = 352
+  ClientWidth = 638
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 16
+    Top = 24
+    Width = 24
+    Height = 13
+    Caption = #36134#25143
+  end
+  object Label2: TLabel
+    Left = 200
+    Top = 24
+    Width = 24
+    Height = 13
+    Caption = #23494#30721
+  end
+  object Label3: TLabel
+    Left = 16
+    Top = 68
+    Width = 24
+    Height = 13
+    Caption = #36135#21495
+  end
+  object Label4: TLabel
+    Left = 200
+    Top = 68
+    Width = 24
+    Height = 13
+    Caption = #23610#30721
+  end
+  object Edit1: TEdit
+    Left = 46
+    Top = 21
+    Width = 121
+    Height = 21
+    TabOrder = 0
+    Text = 'only4reg@126.com'
+  end
+  object Edit2: TEdit
+    Left = 230
+    Top = 21
+    Width = 121
+    Height = 21
+    PasswordChar = '*'
+    TabOrder = 1
+    Text = 'NikePlus360'
+  end
+  object Edit3: TEdit
+    Left = 46
+    Top = 65
+    Width = 121
+    Height = 21
+    TabOrder = 2
+    Text = '10091109'
+  end
+  object Memo1: TMemo
+    Left = 16
+    Top = 112
+    Width = 609
+    Height = 201
+    Lines.Strings = (
+      'Memo1')
+    ScrollBars = ssVertical
+    TabOrder = 3
+  end
+  object Button1: TButton
+    Left = 388
+    Top = 38
+    Width = 61
+    Height = 25
+    Caption = #36141#20080
+    TabOrder = 4
+    OnClick = Button1Click
+  end
+  object Edit4: TEdit
+    Left = 230
+    Top = 65
+    Width = 121
+    Height = 21
+    TabOrder = 5
+    Text = 'Edit4'
+  end
+end
